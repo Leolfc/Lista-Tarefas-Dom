0d3d8b0e7368698868b1edcb7134f171dbd44a67
Leolfc <leonardofeliciano81@gmail.com>
2025-03-27 13:59:27 -0300

    Deixando o codigo mais organizado

--- projeto_inicial/listaTarefas.js
+++ projeto_inicial/listaTarefas.js
@@ -1,42 +1,24 @@
-(() => {
-  const criarTarefa = (evento) => {
-    evento.preventDefault();
+import BotaoConclui from "../componentes/concluiTarefa.js";
+import BotaoDeleta from "../componentes/deletaTarefa.js";
+const criarTarefa = (evento) => {
+  evento.preventDefault();
 
-    const lista = document.querySelector("[data-list]");
-    const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
+  const lista = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
 
-    const tarefa = document.createElement("li");
-    tarefa.classList.add("task");
-    const conteudo = `<p class="content">${valor}</p>`;
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
+  const conteudo = `<p class="content">${valor}</p>`;
 
-    tarefa.innerHTML = conteudo;
+  tarefa.innerHTML = conteudo;
 
-    tarefa.appendChild(BotaoConclui());
-    lista.appendChild(tarefa);
-    input.value = " ";
-  };
+  tarefa.appendChild(BotaoConclui());
+  tarefa.appendChild(BotaoDeleta());
+  lista.appendChild(tarefa);
+  input.value = " ";
+};
 
-  const novaTarefa = document.querySelector("[data-form-button]");
+const novaTarefa = document.querySelector("[data-form-button]");
 
-  novaTarefa.addEventListener("click", criarTarefa);
-
-  const BotaoConclui = () => {
-    const botaoConclui = document.createElement("button");
-
-    botaoConclui.classList.add("check-button");
-    botaoConclui.innerText = "concluir";
-
-    botaoConclui.addEventListener("click", concluirTarefa);
-
-    return botaoConclui;
-  };
-
-  const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target;
-
-    const tarefaCompleta = botaoConclui.parentElement;
-
-    tarefaCompleta.classList.toggle("done");
-  };
-})();
+novaTarefa.addEventListener("click", criarTarefa);
